c98d14e3a2a9c66931e14692946dcef4882c3d70
Satish Kumar Varma <svarma@griddynamics.com>
2026-05-26 15:57:50 +0530

    first commit

---
+++ HttpRest/src/main/java/org/example/httprest/HttpRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.httprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpRestApplication.class, args);
+    }
+
+}
